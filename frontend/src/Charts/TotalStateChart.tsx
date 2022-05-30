213503ebea21d790dc020125a56b5f469acdb7b7
Jiwon-Choi <jwchoi.py@gmail.com>
2022-05-30 15:58:20 +0900

    :zap: axios 부분에 try-catch 추가
    - *Chart.tsx : 테스트 코드 삭제 및 try-catch문 추가
                   데이터를 불러올 수 없는 경우 차트 대신 렌더링 할 컴포넌트 추가
    - NavBar.tsx : '현황' 메뉴의 이름을 '연체/고장'으로 수정. letter-spacing 디자인에 대해 의논 필요해 보임(...)

--- frontend/src/Charts/TotalStateChart.tsx
+++ frontend/src/Charts/TotalStateChart.tsx
@@ -2,75 +2,54 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
 import { FloorStateData, PieData } from "../type";
+import styled from "styled-components";
 
 const TotalStateChart = () => {
   const [totalState, setTotalState] = useState<PieData[]>([]);
+  const [isError, setIsError] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchState = async () => {
-      // const res = await axios.get(
-      //   "http://localhost:8080/api/cabinet/count/floor"
-      // );
-
-      // 임시 데이터
-      const res = {
-        data: [
-          {
-            floor: "2",
-            total: 148,
-            used: 73,
-            overdue: 36,
-            unused: 39,
-            disabled: 0,
-          },
-          {
-            floor: "4",
-            total: 100,
-            used: 50,
-            overdue: 21,
-            unused: 28,
-            disabled: 1,
-          },
-          {
-            floor: "5",
-            total: 96,
-            used: 40,
-            overdue: 22,
-            unused: 34,
-            disabled: 0,
-          },
-        ],
-      };
-
-      let used = 0,
-        overdue = 0,
-        disabled = 0,
-        unused = 0;
-      res.data.forEach((element: FloorStateData) => {
-        used += element.used;
-        overdue += element.overdue;
-        disabled += element.disabled;
-        unused += element.unused;
-      });
-      setTotalState([
-        { name: "사용 중", value: used },
-        { name: "연체", value: overdue },
-        { name: "사용 불가", value: disabled },
-        { name: "미사용", value: unused },
-      ]);
+      try {
+        const res = await axios.get(
+          "http://localhost:8080/api/cabinet/count/floor"
+        );
+        let used = 0,
+          overdue = 0,
+          disabled = 0,
+          unused = 0;
+        res.data.forEach((element: FloorStateData) => {
+          used += element.used;
+          overdue += element.overdue;
+          disabled += element.disabled;
+          unused += element.unused;
+        });
+        setTotalState([
+          { name: "사용 중", value: used },
+          { name: "연체", value: overdue },
+          { name: "사용 불가", value: disabled },
+          { name: "미사용", value: unused },
+        ]);
+      } catch (e) {
+        console.log("error");
+        setIsError(true);
+      }
     };
     fetchState();
   }, []);
 
   const colors = ["#05a55a", "#f39c13", "#f56a54", "#bbbbbb"];
 
+  if (isError)
+    return (
+      <DataLoadErrorStyles>데이터를 불러올 수 없습니다 :(</DataLoadErrorStyles>
+    );
   return (
     <PieChart width={500} height={300}>
       <Tooltip cursor={false} contentStyle={{ backgroundColor: "#ffffffaa" }} />
       <Legend />
       <Pie
         data={totalState}
-        // startAngle={180}
-        // endAngle={0}
         innerRadius={70}
         outerRadius={100}
         paddingAngle={1}
@@ -85,5 +64,13 @@ const TotalStateChart = () => {
     </PieChart>
   );
 };
+
+const DataLoadErrorStyles = styled.div`
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default TotalStateChart;
