a55bc7a599bcb39c10a3364ce26d76f1bfe86986
jaesjeon <jaesjeon@student.42seoul.kr>
2022-05-31 20:30:57 +0900

    :sparkles: 로그인 검증 추가 #25

--- frontend/src/Pages/Status.tsx
+++ frontend/src/Pages/Status.tsx
@@ -13,9 +13,14 @@ const Status = () => {
   useEffect(() => {
     const urlExpired = "http://localhost:8080/api/lent/overdue";
     const urlDisabled = "http://localhost:8080/api/activation";
+    const token = localStorage.getItem("accessToken");
 
-    const requestExpired = axios.get(urlExpired);
-    const requestDisabled = axios.get(urlDisabled);
+    const requestExpired = axios.get(urlExpired, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const requestDisabled = axios.get(urlDisabled, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
     axios
       .all([requestExpired, requestDisabled])
