1a43c6ee977b58069c72e714cc4d4f76fc0b175d
yoyoo <mcwatney@naver.com>
2022-06-17 04:36:10 +0900

    :construction: Feat : #59

---
+++ backend/initial-setting/user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL,
+  `intra_id` varchar(30) NOT NULL,
+  `auth` tinyint(4) NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `phone` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
