5d7683e5e4593a138bd202b3250835e5cca2fde6
yubinquitous <yb9854@naver.com>
2022-06-27 18:27:04 +0900

    [FIX] ban 사물함 조회 api 수정(#66)

--- backend/db/query.js
+++ backend/db/query.js
@@ -207,10 +207,9 @@ const getCabinetInfoByFloor = async (connection) => {
 // ban 사물함 정보
 const getBanCabinetList = async (connection) => {
   const content = `
-    SELECT c.floor, c.cabinet_num, (SELECT intra_id FROM user u WHERE u.user_id=ll.log_user_id) as intra_id, ll.return_time
+    SELECT c.floor, c.cabinet_num, c.section
     FROM cabinet c
-    JOIN lent_log ll
-    ON ll.log_cabinet_id = c.cabinet_id AND c.activation=2;
+    WHERE c.activation=2;
     `;
   const result = await connection.query(content);
   return result;
