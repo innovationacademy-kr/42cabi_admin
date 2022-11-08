bfd54320ef094c5da817e2459c87a11f684a7604
aseungbo <seungbo1112@gmail.com>
2022-11-08 14:13:25 +0900

    fe_v3: Add searchResponse, statusDisabled, statusExpired slice

--- frontend/src/redux/slices/taskBanCabinetSlice.ts
+++ frontend/src/redux/slices/taskBanCabinetSlice.ts
@@ -1,15 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { TaskBanCabinetData } from "../../type";
 
-// Define the initial state using that type
 const initialState: TaskBanCabinetData = [];
 
 export const taskBanCabinetSlice = createSlice({
   name: "taskBanCabinet",
-  // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
     GetBanCabinetResponse: (
       state,
       action: PayloadAction<TaskBanCabinetData>
